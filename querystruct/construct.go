76a9dae0314739160dfea8cb0faaa140d9443556
Rasyid Hakim <muhammad.hakim@go-jek.com>
2024-06-30 16:27:53 +0700

    feat: support referenced query

--- querystruct/construct.go
+++ querystruct/construct.go
@@ -11,9 +11,39 @@ type Construct struct {
 	Functions map[string]any
 }
 
+type Renderer interface {
+	Render() (string, error)
+}
+
+type QueryModel = any
+
+type ConstructRenderer Construct
+
+func (c ConstructRenderer) Render() (string, error) {
+	return RenderConstruct(Construct(c), nil)
+}
+
+type ReferenceRenderer interface {
+	Render() (string, error)
+}
+
+type Reference struct {
+	Name      string
+	Reference ReferenceRenderer
+}
+
 // RenderConstruct wrapper on top of generateTemplate for better organization
-func RenderConstruct(construct Construct, queryModel interface{}) (string, error) {
-	return renderTemplate(construct.Template, construct.Functions, queryModel)
+func RenderConstruct(construct Construct, queryModel QueryModel, references ...Reference) (string, error) {
+	if len(references) == 0 {
+		return renderTemplate(construct.Template, construct.Functions, queryModel)
+	}
+
+	for _, reference := range references {
+		// TODO: implement reference render logic
+		_ = reference
+	}
+	// TODO: implement root render logic
+	return "", nil
 }
 
 // renderTemplate is a function helper to create templatize SQL using GO template. The function return the rendered template as text with trimmed new line and spaces. Return any error from executing the template.
