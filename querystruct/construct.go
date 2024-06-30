83494a06488446afff4cbee3c3ccd9ffacfa9db7
Rasyid Hakim <muhammad.hakim@go-jek.com>
2024-06-30 20:51:11 +0700

    feat: implement one level nesting

--- querystruct/construct.go
+++ querystruct/construct.go
@@ -41,12 +41,33 @@ func RenderConstruct(constructRenderer ConstructRenderer, references ...Referenc
 		return constructRenderer.Render()
 	}
 
-	for _, reference := range references {
-		// TODO: implement reference render logic
-		_ = reference
+	t, err := template.New("").
+		Funcs(constructRenderer.Construct.Functions).
+		Parse(constructRenderer.Construct.Template)
+	if err != nil {
+		return "", err
 	}
-	// TODO: implement root render logic
-	return "", nil
+
+	for _, reference := range references {
+		refName := reference.Name
+		partial, err := reference.Reference.Render()
+		if err != nil {
+			return "", fmt.Errorf("fail to render SQL for %s template reference: %w", refName, err)
+		}
+
+		_, err = t.New(refName).Parse(partial)
+		if err != nil {
+			return "", fmt.Errorf("fail to render SQL for %s template reference: %w", refName, err)
+		}
+	}
+
+	out := bytes.Buffer{}
+	err = t.Execute(&out, constructRenderer.QueryModel)
+	if err != nil {
+		return "", fmt.Errorf("fail to render SQL for root template: %w", err)
+	}
+
+	return CleanQuery(out.String()), nil
 }
 
 // renderTemplate is a function helper to create templatize SQL using GO template. The function return the rendered template as text with trimmed new line and spaces. Return any error from executing the template.
