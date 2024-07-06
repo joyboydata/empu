abfbce6dfe796ad558e4e3e1e8d48e08aa8be6d1
Rasyid Hakim <muhammad.hakim@go-jek.com>
2024-07-06 22:59:26 +0700

    doc: add code doc for querystruct

--- querystruct/construct.go
+++ querystruct/construct.go
@@ -6,36 +6,45 @@ import (
 	"text/template"
 )
 
+// Construct defines a SQL template with associated functions for rendering.
 type Construct struct {
 	Template  string
 	Functions map[string]any
 }
 
+// Renderer interface defines a method to render a SQL template.
 type Renderer interface {
 	Render() (string, error)
 }
 
+// QueryModel is an alias for any type, representing the model used in the template rendering.
 type QueryModel = any
 
+// ConstructRenderer renders a Construct with a given QueryModel.
 type ConstructRenderer struct {
-	Construct  Construct
+	// The Construct containing the template and functions
+	Construct Construct
+	// The model data to be used in the template
 	QueryModel any
 }
 
+// Render generates the rendered SQL string from the Construct and QueryModel.
 func (c ConstructRenderer) Render() (string, error) {
 	return renderTemplate(c.Construct.Template, c.Construct.Functions, c.QueryModel)
 }
 
+// ReferenceRenderer interface defines a method to render a reference template.
 type ReferenceRenderer interface {
 	Render() (string, error)
 }
 
+// Reference holds the name and the reference renderer for a sub-template.
 type Reference struct {
 	Name      string
 	Reference ReferenceRenderer
 }
 
-// RenderConstruct wrapper on top of generateTemplate for better organization
+// RenderConstruct renders the main construct with optional references.
 func RenderConstruct(constructRenderer ConstructRenderer, references ...Reference) (string, error) {
 	if len(references) == 0 {
 		return constructRenderer.Render()
--- querystruct/doc.go
+++ querystruct/doc.go
@@ -1,18 +1,81 @@
+/*
+Package querystruct provides a SQL query templating toolkit designed to simplify the creation of data applications.
+Empu leverages Go templates to facilitate the construction of dynamic SQL queries, making it easier to manage and
+reuse SQL code in a structured and efficient manner.
+
+# Overview
+
+Empu aims to simplify the process of creating complex SQL queries by allowing developers to define templates
+for their SQL code. These templates can include placeholders for dynamic values and can be customized with
+functions to handle specific rendering logic. The package supports the following key features:
+
+ 1. SQL Templates: Define SQL queries with placeholders for dynamic values, making it easy to construct queries
+    with varying parameters.
+
+ 2. Template Functions: Extend the capabilities of your SQL templates by defining custom functions to handle
+    specific logic within the templates.
+
+ 3. Query Models: Use any Go data structure as a model to populate the placeholders in your SQL templates,
+    allowing for flexible and dynamic query generation.
+
+ 4. Reference Templates: Manage complex SQL queries by breaking them down into smaller, reusable templates
+    that can be referenced within the main template.
+
+# Package Components
+
+The querystruct package provides several key components to facilitate SQL query templating:
+
+1. Construct: Represents a SQL template with associated functions for rendering.
+
+2. Renderer Interface: Defines the Render method for rendering templates.
+
+3. ConstructRenderer: Implements the Renderer interface, allowing the rendering of a Construct with a given QueryModel.
+
+4. ReferenceRenderer Interface: Defines the Render method for reference templates.
+
+5. Reference: Represents a named reference to a sub-template, enabling modular and reusable SQL query components.
+
+# Usage
+
+To use the querystruct package, define a Construct with your SQL template and any custom functions. Create a
+ConstructRenderer with the Construct and a QueryModel. Then, use the RenderConstruct function to generate
+the final SQL query, optionally including any Reference templates.
+
+Example:
+
+	package main
+
+	import (
+		"fmt"
+		"querystruct"
+	)
+
+	func main() {
+		construct := querystruct.Construct{
+			Template: "SELECT * FROM users WHERE name = {{uppercase .Name}}",
+			Functions: map[string]interface{}{
+				"uppercase": strings.ToUpper,
+			},
+		}
+
+		queryModel := struct {
+			Name int
+		}{
+			Name: 1,
+		}
+
+		renderer := querystruct.ConstructRenderer{
+			Construct:  construct,
+			QueryModel: queryModel,
+		}
+
+		query, err := querystruct.RenderConstruct(renderer)
+		if err != nil {
+			fmt.Println("Error rendering query:", err)
+			return
+		}
+
+		fmt.Println("Rendered query:", query)
+	}
+*/
 package querystruct
-
-// Consideration:
-// - Run time VS compile time render
-// - Composability, reusability, and testability
-// - Inversion of control is required for composability
-// - Run-time metadata injection
-// - Component:
-//   - Construct: deal with the SQL template
-//   - Composer: compose Construct with QueryModel to render the query
-//   - Reference: criteria of reference, it should work by itself
-
-// TODO: Have a robust error handling.
-// Error to handle:
-//   - Expected QueryModel error should contain which template contain error
-//   - If reference is missing it should tell what to do
-
-// TODO: Define unit testing framework. Test should apply to individual Construct
